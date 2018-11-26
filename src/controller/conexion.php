d485f9d4254177e3e91e26436a7159d3dd630d7a
embuckets <mok.boss@hotmail.com>
2018-11-25 21:31:09 -0600

    fix conexion credenciales

--- src/controller/conexion.php
+++ src/controller/conexion.php
@@ -1,7 +1,7 @@
 <?php
    $servername = "localhost";
-   $username = "root";
-   $password = "1234";
+   $username = "equipo";
+   $password = "equipo";
    $dbName = "encuestas";
    $conn = new mysqli($servername, $username, $password, $dbName);
 
